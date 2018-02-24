a2ba718cd028e479b21a970398e1a6faaa38bcf1
Kai Jacobsen <ungestaltbar@gmail.com>
2018-02-24 19:35:22 +0100

    autodeploy

--- build/hash.php
+++ build/hash.php
@@ -3,5 +3,5 @@
 // either returns the current git hash or a random string
 // this will invalidate the local storage upon changes
 function getGitHash(){
-	return "6aaf56ae11ced5bad137a142adeda55825d3643b";
+	return "6058787f937a2deceace515b572930ebfe9c011c";
 };
